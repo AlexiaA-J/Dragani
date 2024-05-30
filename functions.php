24d971c0bcb964555cec06f148e2e9f274d1ec97
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-05-30 17:33:06 +0200

    Enqueue js scripts

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/assets/scss/theme.css' );
+    wp_enqueue_script('menuBurger-script', get_template_directory_uri() . '/assets/js/modules/menuBurger.js', array('jquery'), null, true);
+    wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), null, true);
 }
 
 function logoCustomizer($wp_customize) {
