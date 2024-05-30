1f115f357bcf69d84c2693602e77d15d347a5249
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-05-30 17:31:19 +0200

    Adapt and add footer content

--- footer.php
+++ footer.php
@@ -1,14 +1,24 @@
-        <footer>
-            <div class="modal-overlay">
-            <?php 
-                $current_url = $_SERVER['REQUEST_URI'];
-                if (strpos($current_url, 'eng') !== false) : ?>
-                <?php get_template_part('assets/template-parts/contact-form-eng'); ?>
-            <?php else: ?>
-                <?php get_template_part('assets/template-parts/contact-form-fr'); ?>
-            <?php endif; ?>
+        <footer class="footer">
+           <a class="footer__chambreSyndic" href="https://www.chambre-syndicale-sophrologie.fr/">
+            <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/chambre-syndicale.png'); ?>" alt="Chambre Syndicale de la Sophrologie">
+           </a>
+            <div class="footer__contact">
+                <p class="contactTitle">
+                    Me contacter :
+                </p>
+                <p>
+                    Delphine Dragani <br>
+                    Sophrologue à Montbonnot Saint Martin, Grenoble et ses environs <br> <br>
+                    Tél : +33 (0) 6 98 27 74 11 <br>
+                    Mail : delphinedragani-sophro@outlook.com
+                </p>
+                <div class="legal">
+                    <p>
+                        © 2023 Delphine Dragani
+                    </p>
+                    <?php wp_nav_menu(array('theme_location' => 'footer')); ?>
+                </div>
             </div>
-            <p lang="en">© All Rights Reserved</p>
         </footer>
         <?php wp_footer(); ?>
     </div>
